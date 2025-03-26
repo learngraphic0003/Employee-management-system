9eb07eac43acaa6b5d2fa1f518ff228cb6c89aa1
shubham <learngraphic0003@gmail.com>
2025-03-26 18:34:55 +0530

    all

--- src/components/other/Hearder2.jsx
+++ src/components/other/Hearder2.jsx
@@ -10,11 +10,9 @@ const Header2 = (props) => {
   return (
     <div className="flex flex-col sm:flex-row items-center justify-between p-4 bg-black  shadow-md rounded-md max-w-screen-2xl mx-auto mt-1.5 w-full">
       {/* Admin Greeting */}
+      <div className='flex justify-start'>   
       <div className="text-xl sm:text-2xl font-semibold flex items-center  text-center sm:text-left">
         {/* <span className="text-red-400 font-bold mr-0 text-w">H</span> <span className='text-white'> ello, Admin</span>  */}
-        
-        
-
 <Focus 
 sentence="Hello Shubham"
 manualMode={false}
@@ -23,14 +21,18 @@ borderColor="red"
 animationDuration={2}
 pauseBetweenAnimations={1}
 />
+
         
       </div>
-      
-          <img 
+      <div><img 
             src="https://steamuserimages-a.akamaihd.net/ugc/395550145285361080/7407352D633D9E23EB8EC682D972008591D76932/?imw=5000&amp;imh=5000&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=false" 
             alt="Admin" 
-            className="w-[30%] rounded-full border-2 "
-          />
+            className=" w-[ 30%] rounded-full border-2 "
+          />  </div>
+      
+      </div>
+      
+        
         
 
       {/* Logout Button */}
